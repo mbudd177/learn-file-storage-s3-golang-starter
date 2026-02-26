e727b7621a97fa04c2ad0c348162f2916a5ea85a
mbudd177 <mbudd177@gmail.com>
2026-02-26 14:17:59 -0800

    fix: update handler to store large files on disk in assets directory instead of base64 encoding in database

--- handler_login.go
+++ handler_login.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"github.com/mbudd177/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/mbudd177/learn-file-storage-s3-golang-starter/internal/database"
 )
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
